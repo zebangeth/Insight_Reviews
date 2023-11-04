1232c5fa040f9624c28f08156c7ce7175bd8c55a
zebang.eth <zebang.li.2012@gmail.com>
2023-11-04 01:03:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.set_page_config(page_title="Insightful Reviews", page_icon="⛳️", layout="
 if 'menu_option' not in st.session_state:
     st.session_state['menu_option'] = 'Home'
 
-lang_choice = st.sidebar.selectbox('Choose your language', options=['English', '简体中文'])
+lang_choice = st.sidebar.selectbox('Choose your language', options=['English(beta)', '简体中文'])
 
 language = "en" if lang_choice == 'English' else "zh"
 
